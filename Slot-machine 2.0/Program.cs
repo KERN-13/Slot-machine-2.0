44a92e531d6bd0083ab5d58318d027999af8c43a
Kern Lasocki <kern2501@gmail.com>
2023-04-12 07:50:45 -0700

    test commit

--- Slot-machine 2.0/Program.cs
+++ Slot-machine 2.0/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1
 
             Console.WriteLine("Welcome to the slot machine game!");
             Console.WriteLine($"You have 50 coins to start with.");
-            
+            //test comment
             int coins = 50;
             Random random = new Random();
 
